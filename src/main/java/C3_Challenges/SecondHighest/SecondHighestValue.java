cbab1cea6796c1440b58f987db279cb63f7e3211
tedbeast <tedbalashov@gmail.com>
2023-02-28 13:21:03 -0500

    initial

---
+++ src/main/java/C3_Challenges/SecondHighest/SecondHighestValue.java
@@ -0,0 +1,11 @@
+package C3_Challenges.SecondHighest;
+
+public class SecondHighestValue {
+    /**
+     * @param arr an array of ints.
+     * @return the second highest value of arr.
+     */
+    public int getValue(int[] arr){
+        return 0;
+    }
+}
